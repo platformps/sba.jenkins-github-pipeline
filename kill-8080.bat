98441a1918c1b9dd7e95a3bacae6ae6e720f4c1d
nmm131 <nmm131@gmail.com>
2020-12-21 10:59:02 -0500

    Added 'Procfile'

--- kill-8080.bat
+++ kill-8080.bat
@@ -1,5 +1,7 @@
 for /f "tokens=5" %%a in ('netstat -aon ^| find ":8080" ^| find "LISTENING"') do taskkill /f /pid %%a
 
+for /f "tokens=5" %%a in ('netstat -aon ^| find ":8070" ^| find "LISTENING"') do taskkill /f /pid %%a
+
 for /f "tokens=5" %%a in ('netstat -aon ^| find ":8081" ^| find "LISTENING"') do taskkill /f /pid %%a
 
 for /f "tokens=5" %%a in ('netstat -aon ^| find ":8082" ^| find "LISTENING"') do taskkill /f /pid %%a
